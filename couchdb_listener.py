feeac0889036398d168a271b9f7944d4c135113c
Ubuntu <ubuntu@ubuntu.bast.lan>
2022-04-26 14:00:31 +0000

    cleanup

--- couchdb_listener.py
+++ couchdb_listener.py
@@ -6,7 +6,6 @@ db = couch['gopro_stats']
 
 for changes in db.changes(feed="continuous", heartbeat=50000):
  doc = db[changes["id"]]
- #print("Name is: "+doc['name'])
  print("Data is: "+ str(doc['data'])+"\n")
  print("Data2 is: "+ str(doc['data2'])+"\n")
 
@@ -20,14 +19,6 @@ for changes in db.changes(feed="continuous", heartbeat=50000):
 
 
 
-# print("Datalist is "+str(len(datalist))+" entries long \n")
-# print(datalist[0])
-# print("Type: "+datalist[0]['type']+"\n")
-# print("Data2[1] is: " str(doc['data2'])+"\n")
 
-
-#doc = {'name':'Employee'}
-
-#db.save(doc)
 
 
