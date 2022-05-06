348e0d805072242dccfca357ed0eb465f0eb09c3
MJakobs97 <markus.jakobs@smail.th-koeln.de>
2022-05-06 13:19:45 +0000

    added oled messages

--- couchdb_listener.py
+++ couchdb_listener.py
@@ -1,39 +1,69 @@
 import couchdb
+import sys
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import sh1106
 from time import sleep
 
+
+
 serial = i2c(port=1, address=0x3c)
 
 device = sh1106(serial)
-iter = 0
-while iter < 10:
- with canvas(device) as draw:
-  draw.rectangle(device.bounding_box, outline="white",fill="black")
-  msg = "Hello World " + str(iter)
-  draw.text((30,40),msg, fill="white")
-  iter +=1
-  sleep(10)
+#iter = 0
+#while iter < 3:
+# with canvas(device) as draw:
+#  draw.rectangle(device.bounding_box, outline="white",fill="black")
+#  msg = "Hello World " + str(iter)
+#  draw.text((30,40),msg, fill="white")
+#  iter +=1
+#  sleep(10)
 
 couch = couchdb.Server()
+database_name = "gopro_stats"
 
-db = couch['gopro_stats']
+while database_name not in couch:
+ with canvas(device) as draw:
+  msg = "DB document not initialized!\n Please wait ..."
+  draw.text((15,32),msg, fill="white")
+
+db=""
 
 try:
- for changes in db.changes(feed="continuous", heartbeat=50000):
-  doc = db[changes["id"]]
-  print("Data is: "+ str(doc['data'])+"\n")
-  print("Data2 is: "+ str(doc['data2'])+"\n")
-
-
-  datalist = doc['data2']
-  print("Data2 is "+str(len(datalist))+" items long.\n")
-  for x in range(len(datalist)):
-   print(datalist[x])
-   print("\n")
+ db = couch[database_name]
 except Exception as ex:
- print("Error: \n", ex)
+ with canvas(device) as draw:
+  msg = "DB connection Error!"
+  draw.text((0,32),msg, fill="white")
+  sleep(5)
+  sys.exit("DB connection error, restarting listener...")
+
+with canvas(device) as draw:
+ msg = "DB connection successful!"
+ draw.text((0,32),msg, fill="white")
+
+sleep(5)
+
+id = ""
+
+while id not in db:
+ with canvas(device) as draw:
+  draw.text((0,32),"Currently no suitable\ndata in db", fill="white")
+ sleep(5)
+ for id in db:
+  print(id)
+
+sleep(10)
+try:
+ for changes in db.changes(feed="continuous", heartbeat=50000):
+  doc = db[changes[id]]
+  print("Data is: "+ str(doc['data'][0])+"\n")
+  msg = ""
+
+  sleep(5)
+
+except Exception as ex:
+ print("Error: \n", str(ex))
 
 
 
